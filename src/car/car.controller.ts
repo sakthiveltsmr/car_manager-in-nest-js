dc40a9aa334ea59a517d52efad25ff05ca296e86
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-05-09 10:12:59 +0530

    car.controller

--- src/car/car.controller.ts
+++ src/car/car.controller.ts
@@ -15,25 +15,28 @@ import { CarDto } from './car.dto';
 export class CarController {
   constructor(private carService: CarService) {}
 
-  //get method
   @Get()
-  async getCars() {
+  public async getCars() {
     return this.carService.getCars();
   }
+
   @Post()
-  async postCars(@Body() car: CarDto) {
-    return this.carService.postCars(car);
+  public async postCar(@Body() car: CarDto) {
+    return this.carService.postCar(car);
   }
+
   @Get(':id')
-  async getCarsById(@Param('id') id: number) {
+  public async getCarById(@Param('id') id: number) {
     return this.carService.getCarById(id);
   }
+
   @Delete(':id')
-  async deleteCars(@Param('id') id: number) {
+  public async deleteCarById(@Param('id') id: number) {
     return this.carService.deleteCarById(id);
   }
-  @Put(';id')
-  async putCarsById(@Param('id') id: number, @Query() query) {
+
+  @Put(':id')
+  public async putCarById(@Param('id') id: number, @Query() query) {
     const propertyName = query.property_name;
     const propertyValue = query.property_value;
     return this.carService.putCarById(id, propertyName, propertyValue);
